c226b3659aba30e7e3458f232ff817fddafac367
Ingmar Kole <ikole@student.codam.nl>
2023-09-21 15:16:28 +0200

    fix: bitmap size calculation order of operation error

--- tests/simpleMain.c
+++ tests/simpleMain.c
@@ -2,15 +2,16 @@
 #include <kmalloc/kmalloc.h>
 #include "structure/zone.h"
 
+#include <libkm/io/printf.h>
+
 int main()
 {
+    km_printf("small bitmapSize bits: %d, sizeInIntegers: %d\n", SMALL_BLOCK_BITMAP_SIZE_BITS, BLOCK_BITMAP_SIZE_INTEGERS);
     ZoneHeader* someZone = create_zone(&g_smallAllocationZoneMetadata);
 	
     if (someZone == NULL) {
         return 1;
     }
-	// First bit is set for zone header.
-	// uint32_t bitsSetInBitmap = 1; // 0001
 
     return 0;
 }
